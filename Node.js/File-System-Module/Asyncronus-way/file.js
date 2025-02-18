99e7b31d36d2d91eb0673dd71e199e9b7aa7573f
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-02-18 19:01:19 +0600

    Node js UNDERSTANDING

--- Node.js/File-System-Module/Asyncronus-way/file.js
+++ Node.js/File-System-Module/Asyncronus-way/file.js
@@ -1,18 +1,18 @@
 const fs = require("fs")
 
 
-// reading text asynchronously . This follows error callback patter
+// reading text asynchronously . This follows error callback pattern
 // fs.writeFile(file, data[, options], callback)
-fs.readFile('./texts/read.txt','utf-8', (err,data) =>{
-    if(err){
+fs.readFile('./texts/read.txt', 'utf-8', (err, data) => {
+    if (err) {
         throw Error("ERROR Reading Text")
     }
     console.log(data);
 
     // writing text asynchronously
     // fs.writeFile(file, data[, options], callback)
-    fs.writeFile('./texts/write.txt', data, 'utf-8',(err)=>{
-        if(err){
+    fs.writeFile('./texts/write.txt', data, 'utf-8', (err) => {
+        if (err) {
             throw Error("Error Writing Data")
         }
     })
