23e639fdd56538a0b1ca263ec6687391d9d6a3f5
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-02-20 16:53:28 +0600

    Event Emmiter Understanding

--- Node.js/Event-Emitter/event-emitter.js
+++ Node.js/Event-Emitter/event-emitter.js
@@ -1,24 +1,31 @@
 // event-emitter.js
-// emitter.emit(eventName[, ...args])
+// This file demonstrates how to use the Node.js EventEmitter class to create and handle events.
 
-const EventEmitter = require("events")
+const EventEmitter = require("events");
 
-//  as event emitter is a special instance object so we have to create instance 
-const myEmitter = new EventEmitter ()
+// EventEmitter is a special class in Node.js used for handling events.
+// Since EventEmitter is a class, we need to create an instance of it.
+const myEmitter = new EventEmitter();
 
-// create a listener
+// Creating event listeners (subscribers)
 
-// .on means we are waiting j when it will happen. 
-myEmitter.on('birthday', ()=>{
-    console.log("Happy Birthday To You");
-})
+// The .on() method is used to register event listeners.
+// It means "listen for this event and execute the provided callback function when it occurs."
 
-myEmitter.on('birthday', (gift)=>{
-    console.log(`I will Send a ${gift}`);
-})
+// First listener for the 'birthday' event
+myEmitter.on('birthday', () => {
+    console.log("Happy Birthday To You");  // This message will be logged when the event is emitted.
+});
 
-// to make this happen we have to use emitter
-// emitter.emit(eventName[, ...args])
-myEmitter.emit('birthday', 'watch')
-// whenever the birthday is emitted the event listener will be called
-// here watch is gift parameter which will be received in second listener
+// Second listener for the 'birthday' event
+myEmitter.on('birthday', (gift) => {
+    console.log(`I will send a ${gift}`);  // This listener takes an argument 'gift' and logs it.
+});
+
+// Emitting (triggering) the 'birthday' event with an argument
+// The emit() method triggers the event, executing all registered listeners for that event.
+myEmitter.emit('birthday', 'watch');
+
+// Explanation:
+// - The first listener will run and print "Happy Birthday To You".
+// - The second listener will receive 'watch' as the argument and print "I will send a watch".
