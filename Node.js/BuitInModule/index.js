ad76e1cd81a9188d7e3c71ff3b198d8af8008e68
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-02-20 09:45:08 +0600

    Builin Modules Of Node.js

--- Node.js/BuitInModule/index.js
+++ Node.js/BuitInModule/index.js
@@ -1,25 +1,30 @@
 // index.js
 
-// Builtin Modules
+// Built-in Modules
+const path = require("path"); // Import the 'path' module, which provides utilities for working with file and directory paths
 
-const path = require("path")
-
-// path.dirname(path)
+// Example 1: Get the directory name of a given file path
 console.log(path.dirname("/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule/index.js"));
-// this will give the directory /D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule
+// Output: "/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule"
+// The `dirname()` method extracts and returns the directory portion of the specified file path
 
+// Example 2: Parse a file path into an object containing details about its components
 console.log(path.parse("/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule/index.js"));
-// this will parse everything 
+/* Output:
+{
+    root: '/',  // The root directory of the path
+    dir: '/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule', // The directory path excluding the file
+    base: 'index.js', // The file name with extension
+    ext: '.js', // The file extension
+    name: 'index' // The file name without extension
+}
+*/
 
-// output will be 
-// {
-//     root: '/',
-//     dir: '/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule',
-//     base: 'index.js',
-//     ext: '.js',
-//     name: 'index'
-//   }
-
-
-// this works with joining paths
-path.join('/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule/', 'local-1.js');
+// Example 3: Join paths dynamically to create a full file path
+const filePath = path.join(
+    "/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule/",
+    "local-1.js"
+);
+console.log(filePath);
+// Output: "/D:/WORK/LEVEL-2/Mission-2/Module-3-Mastering-Foundatin-of-Expressjs/BuitInModule/local-1.js"
+// The `join()` method intelligently concatenates paths, automatically handling slashes and ensuring a valid path structure
